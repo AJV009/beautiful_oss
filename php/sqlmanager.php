3a338729266db0eef3c8112b9d3a37de9e865d83
Alphons Jaimon <jaimonalphons@gmail.com>
2020-07-31 15:13:36 +0530

    prepared_sql_func_dummy

--- php/sqlmanager.php
+++ php/sqlmanager.php
@@ -6,4 +6,8 @@ function test_input($data) {
     return $data;
 }
 $mysqli = new mysqli("localhost","root","","boss");
+function exequery($query){
+    $mysqli = new mysqli("localhost","root","","boss");
+    // TODO: SQL Injection Safe Query Builder and executer.
+}
 ?>
