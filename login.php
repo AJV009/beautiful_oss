a9e0190be0ad017eef9f0fb913e56d763d929feb
Alphons Jaimon <jaimonalphons@gmail.com>
2020-08-02 16:34:20 +0530

    csrf_broken

--- login.php
+++ login.php
@@ -16,6 +16,7 @@ pageHead('Login');
 				<form class="w3-container" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
  					<h4 class="w3-left w3-margin-left">Username</h4><input class="w3-input w3-border w3-round-xxlarge" type="text" name="username" required>
     				<h4 class="w3-left w3-margin-left">Password</h4><input class="w3-input w3-border w3-round-xxlarge" type="password" name="password" required>
+                    <input type="hidden" name="tkn" value="<?php $token ?>">
     				<input class="w3-btn w3-green w3-round-xxlarge w3-margin" type="submit" value="Login ⚡" name="signin">
  				</form>
  				<h3> OR </h3>
@@ -28,6 +29,7 @@ pageHead('Login');
                         <input class="w3-input w3-border w3-round-xxlarge" name="email" required>
                     <h4 style="display: inline;" class="w3-left w3-margin-left">Password</h4><h6 >1 Lower and Upper case character, 1 number, 1 special character and at least 6 to 50 characters long</h6>
                         <input class="w3-input w3-border w3-round-xxlarge" type="password" name="password" required>
+                        <input type="hidden" name="tkn" value="<?php $token ?>">
     				<input class="w3-btn w3-green w3-round-xxlarge w3-margin" type="submit" value="Signup 😎" name="signup">
  				</form>
 			</div>
@@ -36,7 +38,7 @@ pageHead('Login');
 <?php
 pageFoot();
 $username = $email = $password = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST" & anticsrf(1)) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" & anticsrf(1, $post)) {
     $username = test_input($_POST["username"]);
     $password = test_input($_POST["password"]);
     $pass = "$username"."$password";
--- php/csrf.php
+++ php/csrf.php
@@ -1,11 +1,11 @@
 <?php
 $token=0;
-function anticsrf($flag=3){
+function anticsrf($flag=3,$posttoken='0'){
     if ($flag==0 & !isset($_SESSION['token'])) { $_SESSION['token'] = bin2hex(random_bytes(32));
         $GLOBALS['token'] = $_SESSION['token']; }
-    if ($flag==1 & hash_equals($_SESSION['token'], $_POST['token']) ) return TRUE;
-    else return FALSE;
-    if ($flag==2) { $_SESSION['token'] = bin2hex(random_bytes(32));
+    else if ($flag==1 & hash_equals($_SESSION['token'], $posttoken) ) {return TRUE; unset($_SESSION['token']); }
+    else if ($flag==2) { $_SESSION['token'] = bin2hex(random_bytes(32));
         $GLOBALS['token'] = $_SESSION['token']; }
+        else return FALSE;
 }
 ?>
