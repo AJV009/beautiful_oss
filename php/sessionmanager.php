8d17f578599327600a95ff65008a91a808433def
Alphons Jaimon <jaimonalphons@gmail.com>
2020-08-02 22:24:06 +0530

    session_timeout_fix

--- php/sessionmanager.php
+++ php/sessionmanager.php
@@ -5,7 +5,7 @@ function sessionCheck(){ if(!isset($_SESSION['uid'])) phploc('index.php'); }
 function sessionTime(){
     $time = time();
     if(isset($_SESSION['lastact']) && ($time-$_SESSION['lastact'])>1800) {
-        session_unset();
-        session_destroy(); }
+        session_unset();}
     $_SESSION['lastact'] = $time;
 }
+sessionTime();
